f7ea5715590a63afb1015ec41ba177c58099cdbf
DharaJoshi03 <119690359+DharaJoshi03@users.noreply.github.com>
2026-03-09 10:43:28 +0000

    Added class function code

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,6 +2,33 @@
 #include <string>
 using namespace std;
 
+class LibrarySystem{
+private:
+	string password;
+
+public:
+		LibrarySystem(string pass) : password(pass) {}
+	bool authenticate(string inputPassword) {
+		return inputPassword == password;
+	}
+	string setPassword(string newPassword) {
+		password = newPassword;
+		return "Password created successfully.";
+	}
+	string setusername(string newUsername) {
+		// Assuming we have a username variable to set
+		// username = newUsername;
+		return "Username created successfully.";
+	}
+	string getUsername() {
+		// Assuming we have a username variable to return
+		// return username;
+		return "Username retrieval is implemented.";
+	}
+	string getPassword() {
+		return password;
+	}
+};
 int main()
 {
     std::cout << "Hello World!\n";
